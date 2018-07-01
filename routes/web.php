b60d65145f7d4531cec917d74197243275133cbe
zhangwei4096 <zhangwei4096@gmail.com>
2018-07-01 17:33:39 +0800

    更新我的订单功能

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,161 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'admin','namespace'=>'Admin'],function (){
+    Route::get('login','LoginController@index');
+    Route::get('code','LoginController@code');
+    //Route::get('admin/getcode','LoginController@getcode');
+    Route::post('login/posts','LoginController@posts'); //后台登陆验证
+
+
 });
 
 
-Route::get('admin/login','Admin\LoginController@index');
-Route::get('admin/code','Admin\LoginController@code');
-//Route::get('admin/getcode','Admin\LoginController@getcode');
-Route::post('admin/login/posts','Admin\LoginController@posts'); //后台登陆验证
-
-Route::group(['middleware'=>['admin.login']],function(){
+Route::group(['prefix' => 'admin','namespace'=>'Admin','middleware'=>['admin.login']],function(){
 //添加验证
-    Route::get('admin/index','Admin\IndexController@index'); //后台首页
-    Route::post('admin/posts','Admin\IndexController@posts'); //管理员密码修改
-    Route::get('admin/out','Admin\IndexController@out'); //退出登录
+    Route::get('index','IndexController@index'); //后台首页
+    Route::post('posts','IndexController@posts'); //管理员密码修改
+    Route::get('out','IndexController@out'); //退出登录
 
     //产品管理
-    Route::get('admin/product/list','Admin\ProductController@index'); //产品展示首页
-    Route::get('admin/product/add','Admin\ProductController@add'); //添加产品页面
-    Route::post('admin/product/posts','Admin\ProductController@posts');//添加产品业务逻辑
+    Route::get('product/list','ProductController@index'); //产品展示首页
+    Route::get('product/get','ProductController@get');  //异步数据请求接口
+    Route::get('product/add','ProductController@add'); //添加产品页面
+    Route::post('product/posts','ProductController@posts');//添加产品业务逻辑
+    Route::get('product/update','ProductController@update'); //产品修改展示页面
+    Route::post('product/edit','ProductController@edit'); //产品数据修改
+    Route::any('product/delete','ProductController@delete'); //删除数据
+    Route::any('product/type','ProductController@type'); //上下架商品
+    Route::post('file/upload','ProductController@upload'); //缩略图上传
 
-    Route::post('admin/file/upload','Admin\ProductController@upload'); //缩略图上传
+    //店铺管理
+    Route::get('shop/index','ShopController@index');
+    Route::get('shop/add',function(){
+        return view('Admin.shop.add');
+    });
+    Route::post('shop/posts','ShopController@posts');
+    Route::get('shop/get','ShopController@get');
+    Route::post('shop/delete','ShopController@delete');
+    Route::post('shop/update','ShopController@update');
+    Route::post('shop/scerweima','ShopController@scerweima'); //生成二维码
+    Route::get('shop/{id}',function($id){
+        return $id;  //测试的
+    });
+
+
+    //会员管理
+    Route::get('member/index',function(){
+        return view('Admin.member.index');
+    });
+    Route::get('member/get','MemberController@get');
+    Route::post('member/delete','MemberController@delete');
+    Route::post('member/state','MemberController@state');
+
+    //订单管理
+    Route::get('order/index',function(){
+        return view('Admin.order.index');
+    });
+
+    Route::get('order/get','OrderController@get'); //获取所有用户的订单数据
+    Route::get('order/info/{id}','OrderController@info'); //获取订单里面的信息
+    Route::post('/order/edit','OrderController@edit'); //修改订单
+    Route::post('/order/delete','OrderController@delete'); //删除订单
+    //系统设置
+    Route::get('sys/index',function(){
+        return view('Admin.system.index');
+    });
+    Route::post('sys/save/{type}','SystemController@save');
+    Route::post('sys/get/{type}','SystemController@get'); //获取数据
 });
+
+
+
+//首页路由
+
+
+Route::get('index/verify',function(){
+    //强制跳转到微信授权页面
+    header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxeba0c3cbc3992a59&redirect_uri=http%3A%2F%2Fwww.vimx.cn&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect");
+});
+
+
+Route::group(['namespace'=>'Home'],function(){
+    Route::get('/','IndexController@index',function(\Illuminate\Http\Request $request){
+        //微信商城首页用户登陆
+        //$info  = new \App\Http\Controllers\Home\GetController();
+        //$openid= $info->get_webuser_info($request->post('code'),$request->post('state'));  //吧CODE传递过去拿到 openID
+
+        //开启全局SESSION
+        //session(['openid'=>$openid]);
+
+        //显示模板
+
+    });
+    Route::post('/to_cart','IndexController@to_cart'); //加入到购物车
+    Route::post('/del_cart','IndexController@del_cart'); //删除购物车
+    Route::post('/sum_cart','IndexController@sum_cart'); //购物车减一
+    Route::get('/cart','IndexController@cart'); //购物车展示
+    Route::get('/order/cid/{cid}','IndexController@order');  //填写订单);
+    Route::get('/add/addr',function (){  //添加地址页面
+        return view('Home.index.son.addr');
+    });
+    Route::get('/select/addr','IndexController@select_addr');//选择收货地址
+    Route::post('/add/addr','IndexController@add_addr');//添加收货地址
+    Route::post('/to_order','IndexController@to_order'); //提交订单
+
+    //收货地址操作
+    Route::post('/del/addr','IndexController@del_addr');
+    Route::post('/default/addr','IndexController@set_default'); //设置为默认收货地址
+    Route::get('/edit/addr/{id}','IndexController@edit_addr');  //修改收货地址
+    Route::post('/edit/addr','IndexController@update');
+
+    //我的订单中心操作
+    Route::get('/my/orders','OrderController@index');  //订单中心首页
+    Route::get('/my/orders/{id}','OrderController@more'); //订单详细页面
+});
+
+
+
+
+
+
+
+//下面是测试的
+
+Route::group(['middleware'=>['home.login']],function(){
+    //用户登陆成功
+
+    Route::get('/a',function(){
+       return session('openid');
+    });
+});
+
+//
+Route::get('get/access_token','Home\GetController@access_token');
+Route::get('get/info/{code}','Home\GetController@get_webuser_info');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
